e4d419606b162f710612d040e4faf499de1dee3b
renato <elson.renato.de.franca@gmail.com>
2018-01-12 07:55:52 -0200

    How to generate classes from XML schema with XJC plugin - Demo 02 - first commit

---
+++ GenerateClassesFromSchema02_XJCPlugin/src/main/java/com/bharath/trainings/jaxb/JAXBDemo.java
@@ -0,0 +1,54 @@
+package com.bharath.trainings.jaxb;
+
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import com.bharatthippireddy.patient.Gender;
+import com.bharatthippireddy.patient.Insurance;
+import com.bharatthippireddy.patient.Patient;
+import com.bharatthippireddy.patient.PaymentType;
+
+public class JAXBDemo {
+
+	public static void main(String[] args) {
+
+		try {
+			JAXBContext context = JAXBContext.newInstance(Patient.class);
+			Marshaller marshaller = context.createMarshaller();
+			Insurance insurance = new Insurance();
+			insurance.setLimit(1000);
+			insurance.setProvider("Golden Cross");
+			
+			PaymentType paymentType = new PaymentType();
+			paymentType.setCash(100);
+			paymentType.setInsurance(insurance);
+			
+			Patient patient = new Patient();
+			patient.setId(123);
+			patient.setName("Farofa");
+			patient.setGender(Gender.F);
+			patient.setPhone("+5531646711");
+			patient.setAge(1);
+			patient.setPayment(paymentType);
+
+			StringWriter writer = new StringWriter();
+			marshaller.marshal(patient, writer);
+
+			System.out.println(writer.toString());
+
+			Unmarshaller unMarshaller = context.createUnmarshaller();
+
+			Patient patientResult = (Patient) unMarshaller.unmarshal(new StringReader(writer.toString()));
+			System.out.print(patientResult.getName());
+
+		} catch (JAXBException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
